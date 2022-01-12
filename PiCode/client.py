69ed31128c538bc9c85110654374b1b11b9864dd
gajavegr <gajavegr@rose-hulman.edu>
2022-01-12 15:49:13 -0500

    useful client.py

--- PiCode/client.py
+++ PiCode/client.py
@@ -4,11 +4,16 @@ import socket
 
 s = socket.socket()
 
-HOST = '137.112.44.139'  # The server's hostname or IP address
-PORT = 12345        # The port used by the server
+# HOST = '137.112.44.139'  # The server's hostname or IP address
+HOST = '137.112.233.12'  # The server's hostname or IP address
+PORT = 8080        # The port used by the server
 
 s.connect((HOST, PORT))
-
-print("Received ", s.recv(1024).decode())
+msg = ""
+while True:
+    newmsg = s.recv(1024).decode()
+    if (newmsg is not msg):
+        msg = newmsg
+        print("Received ", msg)
 
 s.close()
